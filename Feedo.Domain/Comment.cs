30d2902c0dad6663989423ff17e650817a81a260
Dan Kyungu <33684517+DanKyungu@users.noreply.github.com>
2022-01-03 12:33:18 +0200

    Add project files.

---
+++ Feedo.Domain/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Feedo.Domain
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string? OriginalComment { get; set; }
+        public string? Username { get; set; }
+        public string? UserFullName { get; set; }
+        public int SocialNetworkId { get; set; }
+        public SocialNetwork? SocialNetwork { get; set; }
+    }
+}
---
+++ Feedo.Domain/SocialNetwork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedo.Domain
+{
+    public class SocialNetwork
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
