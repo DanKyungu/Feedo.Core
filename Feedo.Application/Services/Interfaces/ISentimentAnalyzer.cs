49a80e2eeeb3fb3c15e6e988416c5ee21b6cb337
Dan Kyungu <33684517+DanKyungu@users.noreply.github.com>
2022-01-24 17:25:58 +0200

    Add ISentimentAnalyzer

---
+++ Feedo.Application/Services/Interfaces/ISentimentAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using Feedo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedo.Application.Services.Interfaces
+{
+    public interface ISentimentAnalyzer
+    {
+        Task<SentimentResult> GetSentimentScore(string content);
+    }
+}
---
+++ Feedo.Domain/Models/SentimentResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedo.Domain.Models
+{
+    public class SentimentResult
+    {
+        public double Score { get; set; }
+        public int Sentiment { get; set; }
+    }
+}
